1c4d0a730cacd4689fab4a4353bff3f7f06f1dfc
Prorock201 <prorock@list.ru>
2014-02-17 12:33:47 +0200

    3.1 "Wrong-counter badly works"

--- raining symbols.js
+++ raining symbols.js
@@ -1,13 +1,36 @@
 window.onload = bindEventHandlers;
 var randomSymbol = '';
+var Correct = 0;
+var Wrong = 0;
+var Missed = 0;
 
 function bindEventHandlers() {
-	setInterval(getRain, 100);
+	setInterval(getRain, 1000);
+	$(window).on("keypress", bindUsersKeypress);
+}
+
+function bindUsersKeypress(event) {
+	$(".drops").each(function(index, element) {
+		var cocs = String.fromCharCode(event.which);
+		if ($(element).text() == cocs) {
+			$(".results__body__correct-value").text(Correct += 1);
+			$(element).text(cocs).remove();
+		} else if ($(element).text() != cocs) {
+			$(".results__body__wrong-value").text(Wrong += 1);
+		}
+		$("#input").val(cocs);
+	});
 }
 
 function getRain() {
 	randomString();
-	$("<div>"+randomSymbol+"</div>").prependTo(".content").addClass("drops").css("animation-duration", getRandom(5,20)+"s").css("margin-left", getRandom(65,1200)+"px");
+	$("<div>"+randomSymbol+"</div>").prependTo(".content").addClass("drops").css("animation-duration", getRandom(10,30)+"s").css("margin-left", getRandom(65,1200)+"px");
+	$(".drops").each(function(index, element){
+		if (parseInt($(element).offset().top) > 500) {
+			$(".results__body__missed-value").text(Missed += 1);
+			$(this).remove();
+		}
+	});
 }
 
 function randomString() {
